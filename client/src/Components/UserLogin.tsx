82b067421414febfa26f6ea8744fdb754b6b92cf
uğur hikmet köse <61878338+elacays@users.noreply.github.com>
2022-09-01 22:32:14 +0300

    State Düzenlemesi
    State ve type eklendi
    inputlardaki veri state yazdırıldı

--- client/src/Components/UserLogin.tsx
+++ client/src/Components/UserLogin.tsx
@@ -1,6 +1,17 @@
+import { useEffect, useState } from 'react'
 import {Link} from 'react-router-dom'
 
+type User={
+	eMail?:string,
+	userPass?:string
+}
 function UserLogin() {
+	const [userValue,setUserValue]=useState<User>({})
+
+	useEffect(() => {
+		// tarayıcının başlık bölümünü değiştirmemizi sağlar
+		console.log(userValue)
+	  },[userValue]);
   return (
     <div className="container">
 	<div className="screen">
@@ -8,10 +19,10 @@ function UserLogin() {
         <h1 className='form_name'>LOGIN</h1>
 			<form className="login">
 				<div className="login_field">
-					<input type="text" className="login_input" placeholder="Email"/>
+					<input type="text" className="login_input" placeholder="Email" onChange={e=>setUserValue({...userValue,eMail:e.target.value})}/>
 				</div>
 				<div className="login_field">
-					<input type="password" className="login_input" placeholder="Password"/>
+					<input type="password" className="login_input" placeholder="Password" onChange={e=>setUserValue({...userValue,userPass:e.target.value})}/>
 				</div>
 				<button className="button login_submit">
 					<span className="button_text">Log In Now</span>
--- client/src/Components/UserRegister.tsx
+++ client/src/Components/UserRegister.tsx
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {Link} from 'react-router-dom'
+
+
+type User = {
+	id?:number,
+	eMail?:string,
+	userName?:string,
+	userPass?:string
+
+}
+
+
+
 function UserRegister() {
+	const [userValue,setUserValue]=useState<User>({})
+
+	useEffect(() => {
+		// tarayıcının başlık bölümünü değiştirmemizi sağlar
+		console.log(userValue)
+	  },[userValue]);
   return (
     <div className="container">
 	<div className="screen">
@@ -8,13 +26,13 @@ function UserRegister() {
         <h1 className='form_name'>Register</h1>
 			<form className="login">
 				<div className="login_field">
-					<input type="text" className="login_input" placeholder="Email"/>
+					<input type="text" className="login_input" placeholder="Email" onChange={e=>setUserValue({...userValue,eMail:e.target.value})}/>
 				</div>
 				<div className="login_field">
-					<input type="text" className="login_input" placeholder="User Name"/>
+					<input type="text" className="login_input" placeholder="User Name" onChange={e=>setUserValue({...userValue,userName:e.target.value})}/>
 				</div>
 				<div className="login_field">
-					<input type="password" className="login_input" placeholder="Password"/>
+					<input type="password" className="login_input" placeholder="Password" onChange={e=>setUserValue({...userValue,userPass:e.target.value})}/>
 				</div>
 				<div className="login_field">
 					<input type="password" className="login_input" placeholder="Password"/>
